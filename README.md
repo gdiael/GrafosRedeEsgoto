2152522c0a47bb9425d91400e7f53b633b5605a2
Gdiael Souto Barros <gdiael.barros@gmail.com>
2022-07-14 03:44:57 -0300

    adicionado instruções e leitura de arquivo

--- README.md
+++ README.md
@@ -18,9 +18,15 @@ Para compilar e rodar basta executar os seguintes comandos no terminal
 javac -d ./bin ./src/Main.java
 java -cp bin Main
 ~~~
-Também é possível executar o comando utilizando o 
-Para rodar foi utilizado o utilitário makefile, a versão para windows está disponível no utilitário MSYS2:
+Para rodar foi utilizado o utilitário makefile, a versão para windows está disponível no utilitário MinGW:
 ~~~
-https://www.msys2.org/
+https://sourceforge.net/projects/mingw/
 ~~~
-Após a seguir todos os passo corretamente da instalação é possível usar o comando make no windows.
+Após a seguir todos os passo corretamente da instalação e configuração é possível usar o comando make no windows.
+~~~javascript
+// para compilar e executar
+make runwin
+// apenas para executar
+make run
+~~~
+A linha de comando irá perguntar o nome do arquivo de grafo a ser carregado. Basta digitar o nome de um dos arquivos disponíveis na pasta 'data', ou teclar enter, para usar o arquivo 'demo.txt', o grafo de saída será salvo com o mesmo nome, mas na pasta 'dataout'.
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("Iniciando teste da classe Node");
-        System.out.println("Vamos criar um Node com o valor 4 e imprimi-lo");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("");
+            System.out.println("Iniciando Grafo para Redes de Esgoto.");
+            System.out.println("Digite o nome de um dos arquivos de grafo disponiveis na pasta 'data' [ou Enter para demo]:");
+            
+            String fileName = "demo";
+            
+            String input = scanner.nextLine();
+            if(!input.isEmpty()) fileName = input;
+
+            String prefix = ".\\data\\";
+            String suffix = ".txt";
+
+            if(!fileName.startsWith(prefix)) fileName = prefix + fileName;
+
+            if(!fileName.endsWith(suffix)) fileName += suffix;
+
+            Path filePath = Path.of(fileName);
+            try {
+                String content = Files.readString(filePath, StandardCharsets.UTF_8);
+                System.out.println(content);
+            } catch (Exception e) {
+                System.out.println("Erro ao ler o arquivo: " + fileName);
+            }
+
+        }
+        
+        
     }
 }
